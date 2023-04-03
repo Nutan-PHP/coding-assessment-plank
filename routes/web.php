36593b0a33181dd79d0b4706268cc9d01337b6df
Massimo Triassi <massimo@plankdesign.com>
2023-04-03 09:54:13 -0400

    feat: #2 add routes to access new quiz endpoints

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/quizzes', [\App\Http\Controllers\QuizController::class, 'index']);
+Route::get('/quiz/{id}', [\App\Http\Controllers\QuizController::class, 'show']);
+Route::post('/quiz', [\App\Http\Controllers\QuizController::class, 'store']);
+Route::post('/attempt', [\App\Http\Controllers\QuizAttemptController::class, 'store']);
+Route::delete('/quiz/{id}', [\App\Http\Controllers\QuizController::class, 'destroy']);
